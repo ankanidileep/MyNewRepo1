15cd7b255cd227230de282512118ea07da02ff6f
ankanidileep <ankanidileep98@gmail.com>
2025-11-13 22:03:23 +0530

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,16 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+//dev-2 push from local
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
